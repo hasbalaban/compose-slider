e6cbb612cc3e9d78aaaa71bcee47caa597d1cbea
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-06-18 15:32:16 +0300

    integration shared transaction

--- app/src/main/java/com/balaban/slider/MainActivity.kt
+++ app/src/main/java/com/balaban/slider/MainActivity.kt
@@ -3,6 +3,15 @@ package com.balaban.slider
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.AnimatedContent
+import androidx.compose.animation.AnimatedContentScope
+import androidx.compose.animation.AnimatedVisibilityScope
+import androidx.compose.animation.BoundsTransform
+import androidx.compose.animation.ExperimentalSharedTransitionApi
+import androidx.compose.animation.SharedTransitionLayout
+import androidx.compose.animation.SharedTransitionScope
+import androidx.compose.animation.fadeIn
+import androidx.compose.animation.fadeOut
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -11,9 +20,11 @@ import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.wrapContentHeight
@@ -22,7 +33,9 @@ import androidx.compose.foundation.pager.HorizontalPager
 import androidx.compose.foundation.pager.PagerDefaults
 import androidx.compose.foundation.pager.PagerSnapDistance
 import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -34,6 +47,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
@@ -53,6 +67,8 @@ import com.balaban.slider.ui.theme.SliderTheme
 import com.google.gson.Gson
 import kotlinx.coroutines.launch
 import kotlin.math.absoluteValue
+import kotlin.random.Random
+import kotlin.random.nextUInt
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -73,46 +89,58 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
-private fun MainNavigation(){
+private fun MainNavigation() {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = Screen.Slider.route){
+    SharedTransitionLayout {
+        NavHost(navController = navController, startDestination = Screen.Slider.route) {
 
-        composable(Screen.Slider.route){
-            MainScreen(
-                onClickedSliderDetail = {
-                    navController.navigate(Screen.SliderItemDetail().createRoute(item = it))
-                }
-            )
-        }
-
-        composable(
-            Screen.SliderItemDetail().route,
-            arguments = listOf(
-                navArgument("item") {
-                    type = NavType.StringType
-                    defaultValue = ""
-                })
-        ) {backStackEntry ->
-            val itemDataString = backStackEntry.arguments?.getString("item")
-            val itemData = Gson().fromJson(itemDataString, SliderItem::class.java)
-
-            itemData?.let {
-                ImageDetail(
-                    sliderItem = it,
-                    onBackClicked = {
-                        navController.popBackStack()
+            composable(Screen.Slider.route) {
+                MainScreen(
+                    this@SharedTransitionLayout,
+                    this@composable,
+                    onClickedSliderDetail = {
+                        navController.navigate(Screen.SliderItemDetail().createRoute(item = it))
                     }
                 )
             }
+
+            composable(
+                Screen.SliderItemDetail().route,
+                arguments = listOf(
+                    navArgument("item") {
+                        type = NavType.StringType
+                        defaultValue = ""
+                    })
+            ) { backStackEntry ->
+                val itemDataString = backStackEntry.arguments?.getString("item")
+                val itemData = Gson().fromJson(itemDataString, SliderItem::class.java)
+
+                itemData?.let {
+                    ImageDetail(
+                        sliderItem = it,
+                        this@SharedTransitionLayout,
+                        this@composable,
+                        onBackClicked = {
+                            navController.popBackStack()
+                        }
+                    )
+                }
+            }
         }
+
     }
+
 }
 
+@OptIn(ExperimentalSharedTransitionApi::class)
 @Composable
 fun MainScreen(
-    onClickedSliderDetail : (SliderItem) -> Unit,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+    onClickedSliderDetail: (sliderItem: SliderItem) -> Unit,
     modifier: Modifier = Modifier
 ) {
     val coroutineScope = rememberCoroutineScope()
@@ -142,17 +170,16 @@ fun MainScreen(
         pagerSnapDistance = PagerSnapDistance.atMost(10)
     )
 
-    Column (
-        modifier = Modifier.wrapContentSize(),
-    ){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+    ) {
         HorizontalPager(
             modifier = Modifier
                 .fillMaxHeight(0.4f),
             state = pagerState,
             flingBehavior = fling,
             contentPadding = PaddingValues(horizontal = 32.dp)
-        ) {pageIndex ->
-
+        ) { pageIndex ->
             Card(
                 Modifier
                     .clickable {
@@ -179,31 +206,164 @@ fun MainScreen(
                         )
                     }
             ) {
-                SliderItem(sliderItem = sliderList[pageIndex])
+
+                SliderItem(
+                    sliderItem = sliderList[pageIndex],
+                    sharedTransitionScope = sharedTransitionScope,
+                    animatedContentScope = animatedContentScope
+                )
+            }
+
+            Row(
+                Modifier
+                    .padding(top = 12.dp)
+                    .wrapContentHeight()
+                    .fillMaxWidth()
+                    .padding(bottom = 8.dp),
+                horizontalArrangement = Arrangement.Center
+            ) {
+                repeat(pagerState.pageCount) { iteration ->
+                    val color =
+                        if (pagerState.currentPage == iteration) Color.Red else Color.LightGray
+                    Box(
+                        modifier = Modifier
+                            .clickable {
+                                coroutineScope.launch {
+                                    pagerState.animateScrollToPage(iteration)
+                                }
+                            }
+                            .padding(2.dp)
+                            .clip(CircleShape)
+                            .background(color)
+                            .size(16.dp)
+                    )
+                }
             }
         }
+    }
 
-        Row(
-            Modifier
-                .padding(top = 12.dp)
-                .wrapContentHeight()
-                .fillMaxWidth()
-                .padding(bottom = 8.dp),
-            horizontalArrangement = Arrangement.Center
+
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+fun SliderItem(
+    sliderItem: SliderItem,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+    modifier: Modifier = Modifier
+) {
+    with(sharedTransitionScope) {
+        Box(
+            modifier = modifier
+                .fillMaxSize(),
+            contentAlignment = Alignment.BottomCenter
         ) {
-            repeat(pagerState.pageCount) { iteration ->
-                val color = if (pagerState.currentPage == iteration) Color.Red else Color.LightGray
-                Box(
+            AsyncImage(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .sharedElement(
+                        rememberSharedContentState(key = "image" + "-" + sliderItem.imageUrl),
+                        animatedVisibilityScope = animatedContentScope,
+                    ),
+                model = sliderItem.imageUrl,
+                contentDescription = "Translated description of what the image contains",
+                contentScale = ContentScale.FillBounds
+            )
+
+            Box(
+                contentAlignment = Alignment.BottomCenter
+            ) {
+                Spacer(
                     modifier = Modifier
-                        .clickable {
-                            coroutineScope.launch {
-                                pagerState.animateScrollToPage(iteration)
-                            }
-                        }
-                        .padding(2.dp)
-                        .clip(CircleShape)
-                        .background(color)
-                        .size(16.dp)
+                        .fillMaxWidth()
+                        .height(32.dp)
+                        .alpha(0.5f)
+                        .background(Color.White)
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 24.dp)
+                        .sharedElement(
+                            rememberSharedContentState(key = "title" + "-" + sliderItem.imageUrl),
+                            animatedVisibilityScope = animatedContentScope,
+                        ),
+                    text = "Photo Title",
+                    fontSize = 24.sp,
+                    color = Color.Black
+                )
+            }
+
+        }
+    }
+
+}
+
+@OptIn(ExperimentalSharedTransitionApi::class)
+@Composable
+private fun ImageDetail(
+    sliderItem: SliderItem,
+    sharedTransitionScope: SharedTransitionScope,
+    animatedContentScope: AnimatedContentScope,
+    onBackClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    val text by remember {
+        mutableStateOf(("Günbatımında Huzurun Rengi\n" +
+                "\n" +
+                "Bu nefes kesen günbatımı manzarası, doğanın büyüleyici gücünü ve huzur veren atmosferini yansıtıyor. Sonsuz gökyüzü ve sakin deniz, günün son ışıklarıyla birleşerek unutulmaz bir an yaratıyor. Doğanın bu büyüsü karşısında kendimi çok şanslı hissediyorum. \uD83C\uDF05✨").repeat(Random.nextInt(3,6)))
+    }
+
+    with(sharedTransitionScope){
+        Column(
+            modifier = Modifier
+                .wrapContentSize()
+                .padding(start = 12.dp, end = 12.dp, top = 12.dp)
+        ) {
+            Image(
+                modifier = modifier
+                    .clickable {
+                        onBackClicked.invoke()
+                    }
+                ,
+                painter = painterResource(id = R.drawable.go_back), contentDescription = "go back"
+            )
+
+            Column(
+                verticalArrangement = Arrangement.Center,
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Column( modifier =
+                Modifier
+                    .padding(top = 8.dp)
+                    .sharedElement(
+                        sharedTransitionScope.rememberSharedContentState(key = "title" + "-" + sliderItem.imageUrl),
+                        animatedVisibilityScope = animatedContentScope,
+                    )
+                ) {
+                    Text(
+                        text = "Photo Title", fontSize = 24.sp
+                    )
+                }
+
+                AsyncImage(
+                    modifier = Modifier
+                        .sharedElement(
+                            rememberSharedContentState(key = "image" + "-" + sliderItem.imageUrl),
+                            animatedVisibilityScope = animatedContentScope,
+                        ),
+                    model = sliderItem.imageUrl,
+                    contentDescription = "Translated description of what the image contains",
+                    contentScale = ContentScale.FillBounds
+                )
+
+                Text(
+                    modifier = Modifier
+                        .padding(top = 12.dp)
+                        .verticalScroll(rememberScrollState()),
+                    text = text,
+                    fontSize = 14.sp
                 )
             }
         }
@@ -211,72 +371,19 @@ fun MainScreen(
 
 }
 
-@Composable
-fun SliderItem(sliderItem : SliderItem, modifier: Modifier = Modifier) {
-
-        Column(
-            modifier = modifier.fillMaxSize(),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            AsyncImage(
-                model = sliderItem.imageUrl,
-                contentDescription = "Translated description of what the image contains",
-                contentScale = ContentScale.FillBounds
-            )
-        }
-}
-
-@Composable
-private fun ImageDetail(
-    sliderItem: SliderItem,
-    onBackClicked : () -> Unit,
-    modifier: Modifier = Modifier){
-    Column(modifier = Modifier
-        .wrapContentSize()
-        .padding(start = 12.dp, end = 12.dp, top = 12.dp)) {
-        Image(
-            modifier = modifier
-                .clickable {
-                    onBackClicked.invoke()
-                },
-            painter = painterResource(id = R.drawable.go_back), contentDescription = "go back"
-        )
-
-        Column(
-            modifier = Modifier.padding(top = 36.dp),
-            verticalArrangement = Arrangement.Center,
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            AsyncImage(
-                model = sliderItem.imageUrl,
-                contentDescription = "Translated description of what the image contains",
-                contentScale = ContentScale.FillBounds
-            )
-
-            Text(
-                modifier = Modifier.padding(top = 24.dp),
-                text = "Photo", fontSize = 24.sp
-            )
-        }
-    }
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     SliderTheme {
-        MainScreen(
-            onClickedSliderDetail = {}
-        )
+        MainNavigation()
     }
 }
-
 
 
 fun lerpAlpha(start: Float, stop: Float, fraction: Float): Float {
     return start + fraction * (stop - start)
 }
+
 fun lerpSize(start: Float, stop: Float, fraction: Float): Float {
     return start + fraction * (stop - start)
 }
