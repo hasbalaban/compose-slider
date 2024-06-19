ba3c0171ea67962468bfd904b5e0fd0beecfb1d1
Hasan Balaban <hasan.balaban@demirorenteknoloji.com>
2024-06-19 12:40:41 +0300

    optimize imports

--- app/src/main/java/com/balaban/slider/ui/mainslider/MainScreen.kt
+++ app/src/main/java/com/balaban/slider/ui/mainslider/MainScreen.kt
@@ -1,8 +1,6 @@
 package com.balaban.slider.ui.mainslider
 
-import androidx.compose.animation.AnimatedContentScope
 import androidx.compose.animation.ExperimentalSharedTransitionApi
-import androidx.compose.animation.SharedTransitionScope
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
